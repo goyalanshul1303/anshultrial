a4e6c2832b3b4e55996116cc44ef2a880fda90b9
Anshul Goyal <anshul.goyal@alight.com>
2018-12-21 00:52:51 +0530

    Added Order-service project

---
+++ order-service/src/main/java/com/cartonwale/order/api/dao/OrderDao.java
@@ -0,0 +1,8 @@
+package com.cartonwale.order.api.dao;
+
+import com.cartonwale.common.dao.GenericDao;
+import com.cartonwale.order.api.model.Order;
+
+public interface OrderDao extends GenericDao<Order> {
+
+}
