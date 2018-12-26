543e4138db61b95bc3dd98e2fe46226a28280a64
Anshul Goyal <anshul.goyal@alight.com>
2018-12-26 23:28:41 +0530

    security

--- order-service/src/main/java/com/cartonwale/order/api/controller/OrderController.java
+++ order-service/src/main/java/com/cartonwale/order/api/controller/OrderController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,9 +27,9 @@ public class OrderController extends ControllerBase{
 	
 	@RequestMapping("/consumer")
     public ResponseEntity<List<Order>> getAll() {
-		AuthUser user = (AuthUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
-		System.out.println(user.getUserId());
-		return makeResponse(orderService.getAllByConsumer(user.getUserId()));
+//		AuthUser user = (AuthUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
+//		System.out.println(user.getUserId());
+		return makeResponse(orderService.getAllByConsumer("123"));
     }
 	
 	@RequestMapping("/{id}")
