f7be12726a2dc9e72be157b3f16b0c261da4ab7b
swati aggarwal <aggarwal.swati@snapdeal.com>
2019-01-04 15:40:34 +0530

    added no order screen / last order view

---
+++ mobile/CartonOrdersApplication/app/src/main/java/com/app/carton/orders/NoOrderFragment.java
@@ -0,0 +1,51 @@
+package com.app.carton.orders;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+/**
+ * Created by aggarwal.swati on 1/4/19.
+ */
+
+public class NoOrderFragment extends Fragment implements View.OnClickListener{
+    private  View view;
+    private Button createOrderBtn,repeatOrderBtn;
+    private LinearLayout lastOrderLL, noOrderLL;
+
+    public NoOrderFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.no_order, container, false);
+        initViews();
+        return view;
+    }
+
+    // Initiate Views
+    private void initViews() {
+        createOrderBtn = (Button) view.findViewById(R.id.createOrderBtn);
+        repeatOrderBtn = (Button)view.findViewById(R.id.repeatOrderBtn);
+        noOrderLL = (LinearLayout)view.findViewById(R.id.noOrderLL);
+        lastOrderLL = (LinearLayout)view.findViewById(R.id.lastOrderLL);
+        createOrderBtn.setOnClickListener(this);
+        repeatOrderBtn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
