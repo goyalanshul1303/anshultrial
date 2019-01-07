2049ac28946c4bde52f6bee72d2651ad88af2afd
Anshul Goyal <anshul.goyal@alight.com>
2019-01-07 22:03:57 +0530

    removing dummy project reinstating disabled project for discovery

--- onboarding/provider1/src/main/java/com/cartonwale/provider/api/dao/ProviderDao.java
+++
@@ -1,8 +0,0 @@
-package com.cartonwale.provider.api.dao;
-
-import com.cartonwale.common.dao.GenericDao;
-import com.cartonwale.provider.api.model.Provider;
-
-public interface ProviderDao extends GenericDao<Provider> {
-
-}
--- onboarding/provider1/src/main/java/com/cartonwale/provider/api/service/ProviderService.java
+++
@@ -1,7 +0,0 @@
-package com.cartonwale.provider.api.service;
-
-import com.cartonwale.common.service.GenericService;
-import com.cartonwale.provider.api.model.Provider;
-
-public interface ProviderService extends GenericService<Provider>{
-}
