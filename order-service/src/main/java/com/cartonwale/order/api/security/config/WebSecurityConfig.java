caaf8387becc290da24e45c19fc0e228d6ef9f65
Anshul Goyal <anshul.goyal@alight.com>
2019-04-19 00:13:14 +0530

    Changing url pattern for orderstatus API

--- order-service/src/main/java/com/cartonwale/order/api/security/config/WebSecurityConfig.java
+++ order-service/src/main/java/com/cartonwale/order/api/security/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/orders/provider/completed").hasAnyRole(Permission.USER_PROVIDER, Permission.USER_ADMIN)
                 .antMatchers(HttpMethod.GET, "/orders/**").hasAnyRole(Permission.USER_CONSUMER, Permission.USER_ADMIN, Permission.USER_PROVIDER)
                 .antMatchers(HttpMethod.POST, "/orders/**").hasAnyRole(Permission.USER_CONSUMER, Permission.USER_ADMIN)
-                .antMatchers(HttpMethod.PUT, "/orders/changeOrderStatus").hasAnyRole(Permission.USER_PROVIDER, Permission.USER_ADMIN)
+                .antMatchers(HttpMethod.PUT, "/orders/changeOrderStatus/**").hasAnyRole(Permission.USER_PROVIDER, Permission.USER_ADMIN)
 				.antMatchers(HttpMethod.PUT, "/orders/**").hasAnyRole(Permission.USER_CONSUMER, Permission.USER_ADMIN)
 
 				.antMatchers(HttpMethod.GET, "/quotes/provider").hasAnyRole(Permission.USER_PROVIDER, Permission.USER_ADMIN)
