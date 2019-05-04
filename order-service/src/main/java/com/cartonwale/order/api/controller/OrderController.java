3cb0b040687b0f738e4f9d24362663281ba3cc72
Anshul Goyal <anshul.goyal@alight.com>
2019-05-04 15:39:12 +0530

    code modification

--- order-service/src/main/java/com/cartonwale/order/api/controller/OrderController.java
+++ order-service/src/main/java/com/cartonwale/order/api/controller/OrderController.java
@@ -74,7 +74,7 @@ public class OrderController extends ControllerBase{
 		return makeResponse(orderService.changeStatus(orderId, statusId), HttpStatus.ACCEPTED);
     }
     
-    @RequestMapping(value="/recentOrders")
+    @RequestMapping("/recentOrders")
     public ResponseEntity<List<Order>> recentOrders(@RequestBody List<String> productIds) {
 		return makeResponse(orderService.recentOrders(productIds));
     }
