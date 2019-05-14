c09afd422de7be78fa6c8d142aebeec1d5210968
Anshul Goyal <anshul.goyal@alight.com>
2019-05-14 09:53:51 +0530

    trying projection

--- order-service/src/main/java/com/cartonwale/order/api/dao/impl/OrderDaoImpl.java
+++ order-service/src/main/java/com/cartonwale/order/api/dao/impl/OrderDaoImpl.java
@@ -93,7 +93,7 @@ public class OrderDaoImpl extends GenericDaoImpl<Order> implements OrderDao{
 			
 			Aggregation agg = Aggregation.newAggregation(Aggregation.match(Criteria.where("consumerId").is(consumerId).and("productId").in(productIds))
 					, Aggregation.sort(Direction.DESC, "productId", "orderDate")
-					, Aggregation.group("consumerId", "productId").first("orderDate").as("orderDate").addToSet("orderStatus").as("orderStatus").addToSet("quantity").as("quantity")					
+					, Aggregation.group("consumerId", "productId")				
 					);
 			List<Order> orders = super.getAll(agg);
 			return orders;
