4584e90d3228893a854aba72bd544136fde619cd
Anshul Goyal <anshul.goyal@alight.com>
2019-05-22 00:16:05 +0530

    aggregation trial

--- order-service/src/main/java/com/cartonwale/order/api/dao/impl/OrderDaoImpl.java
+++ order-service/src/main/java/com/cartonwale/order/api/dao/impl/OrderDaoImpl.java
@@ -96,8 +96,8 @@ public class OrderDaoImpl extends GenericDaoImpl<Order> implements OrderDao{
 			
 			Aggregation agg = Aggregation.newAggregation(Aggregation.match(Criteria.where("consumerId").is(consumerId).and("productId").in(productIds))
 					, Aggregation.sort(Direction.DESC, "productId", "orderDate")
-					, Aggregation.group("productId", "_id").first("orderDate").as("orderDate")					
-					, Aggregation.project("orderDate").and("productId").previousOperation());
+					, Aggregation.group("productId").first("orderDate").as("orderDate").first(Aggregation.ROOT).as("doc")					
+					, Aggregation.project("doc"));
 			List<Order> orders = super.getAll(agg);
 			
 			Map<String, Date> productLatestDates = orders.stream().collect(Collectors.toMap(Order::getProductId, Order::getOrderDate));
